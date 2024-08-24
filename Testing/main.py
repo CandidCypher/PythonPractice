d5b933c4c46bc0094c5813dadbcfad96445c535a
Cameron Owens <cameronallanowens@gmail.com>
2024-08-24 12:34:59 -0500

    Adding gitignore and minor changes.

--- Testing/main.py
+++ Testing/main.py
@@ -2,6 +2,8 @@ def add_numbers(num1:int, num2:int)->int:
     """
     Info: Demonstrative function that adds two numbers
     """
+    if not isinstance(num1, int) or not isinstance(num2, int):
+        return TypeError
     return num1 + num2
 
 
--- Testing/test_main.py
+++ Testing/test_main.py
@@ -10,12 +10,23 @@ class TestMain(unittest.TestCase):
         result = main.add_numbers(5,1)
         self.assertEqual(result, 6)
 
+    def test_addition_string(self):
+        """
+        Info: Test that passes a string to get a tpe error.
+        """
+        result = main.add_numbers(5, "string")
+        print(result)
+        self.assertIsInstance(result, TypeError)
+
 
     def test_subtraction(self):
         result = main.subtract_numbers(5,1)
         self.assertEqual(result, 4)
 
     def test_subtraction_fail(self):
+        """
+        Info: Subtraction test.
+        """
         result = main.subtract_numbers(10, 12)
         self.assertNotEqual(result, 0)
 
